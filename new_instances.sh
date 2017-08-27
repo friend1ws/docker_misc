36d4544f48cc50284adf32695b2195648c1de321
Yuichi Shiraishi <friend1ws@gmail.com>
2017-08-27 23:31:30 +0900

    Update new_instances.sh

--- new_instances.sh
+++ new_instances.sh
@@ -4,3 +4,7 @@ gcloud compute instances create savnet-paper3 --zone asia-northeast1-a --machine
 
 gcloud compute instances attach-disk savnet-paper3 --disk savnet-paper-bk170826
 
+
+# STAR
+gcloud compute instances create star-test --zone asia-northeast1-a --machine-type n1-highmem-8 --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud --boot-disk-type pd-ssd --boot-disk-size 200
+
