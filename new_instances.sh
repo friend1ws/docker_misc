8aac7bcdef59af65279b931533474f683094e332
Yuichi Shiraishi <friend1ws@gmail.com>
2017-08-27 18:25:48 +0900

    Update new_instances.sh

--- new_instances.sh
+++ new_instances.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-gcloud compute instances create savnet-paper3 \
-  --zone asia-northeast1-a \
-  --machine-type n1-standard-4 \
-  --boot-disk-device-name ubuntu-cloud-os
+gcloud compute instances create savnet-paper3 --zone asia-northeast1-a --machine-type n1-standard-4 --boot-disk-device-name ubuntu-cloud-os --boot-disk-type pd-ssd --boot-disk-size 200
+
+gcloud compute instances attach-disk savnet-paper3 --disk savnet-paper-bk170826
+
